08c9ac650a19ec91ff39095ff92d2ccbae12a486
Sreesanth R Nair <sreesanthrnair09@gmail.com>
2025-12-16 15:29:35 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
